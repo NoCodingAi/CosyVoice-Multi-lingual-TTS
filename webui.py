5caa8c972232262f31cd5a27d56b2c756d231c40
No Coding Ai <nocodingaisite@gmail.com>
2025-05-31 11:42:06 -0700

    Update webui.py

--- webui.py
+++ webui.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import the
+import os
 import sys
 import argparse
 import gradio as gr
@@ -19,7 +19,7 @@ import numpy as np
 import torch
 import torchaudio
 import random
-import books
+import librosa
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append('{}/third_party/Matcha-TTS'.format(ROOT_DIR))
 from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
